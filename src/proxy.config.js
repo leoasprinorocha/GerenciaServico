0a66346de5f7572a6b79013f2a9f150a7c1dcf3a
LeonardoAsprinoRocha <asprinoufmscg@outlook.com>
2022-07-01 07:41:19 -0400

    Correção link api

--- src/proxy.config.js
+++ src/proxy.config.js
@@ -3,7 +3,7 @@ const proxy = [
     context: "/api",
     target: [
       "http://192.168.1.4:45500/",
-      "https://gerencia-servico-api.herokuapp.com/",
+      "https://gerencia-servico-api.herokuapp.com",
     ],
     pathRewrite: { "^/api": "" },
   },
