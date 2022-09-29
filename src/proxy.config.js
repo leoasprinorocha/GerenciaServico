84a694e7a97afb060921cf71514d14c76887b724
LeonardoAsprinoRocha <asprinoufmscg@outlook.com>
2022-09-28 23:51:15 -0400

    Correção arquivo proxy

--- src/proxy.config.js
+++ src/proxy.config.js
@@ -4,7 +4,8 @@ const proxy = [
     target: [
       "http://192.168.1.4:45500/",
       "https://gerencia-servico-api.herokuapp.com",
-      "http://192.168.137.1:45500"
+      "http://192.168.137.1:45500",
+      "http://170.81.86.141:51000"
     ],
     pathRewrite: { "^/api": "" },
   },
